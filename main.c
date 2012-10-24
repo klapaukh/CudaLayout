142385e90c071584e2961cb76d23d4ba5a6b2794
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2012-10-25 12:10:10 +1300

    Starting XML parsing

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <expat.h>
+
+
+int main(int,char**);
 
 
 int main(int argc, char** argv){
-	printf("Hello, I'm working\n");
+  //Check arguments to make sure you got a file
+  if(argc != 2){
+    printf("Usage: layout filename\n");
+    return EXIT_FAILURE;
+  }
 
-	return EXIT_SUCCESS;
+  //We have a file to read, now lets try to read it
+  XML_Parser p = XML_ParserCreate(NULL); //We do no specify the encoding
+  if(p == NULL){
+    printf("Allocating Memory for parser failed\n");
+    return EXIT_FAILURE;
+  }
+  
+  return EXIT_SUCCESS;
 }
