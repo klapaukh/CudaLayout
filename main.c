2a04223b5aa75822b8b16b9f28cc17224087d7cd
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2012-10-26 10:11:51 +1300

    Starting to link the functionality together

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "graphmlReader.h"
+
 int main(int argc, char** argv){
   //Check arguments to make sure you got a file
   if(argc != 2){
     printf("Usage: layout filename\n");
     return EXIT_FAILURE;
   }
+
+  graph* graph = read(argv[1]);
+
   return EXIT_SUCCESS;
+
+  
 }
