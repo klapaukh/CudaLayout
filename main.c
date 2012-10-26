9031db39a7a7b074b6682d1d7c2280827291c8cf
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2012-10-26 14:32:05 +1300

    Add control flow

--- main.c
+++ main.c
@@ -4,19 +4,27 @@
 #include "graphmlReader.h"
 
 int main(int argc, char** argv){
-  //Check arguments to make sure you got a file
+  /*Check arguments to make sure you got a file*/
   if(argc != 2){
     printf("Usage: layout filename\n");
     return EXIT_FAILURE;
   }
 
-  graph* graph = read(argv[1]);
-  if(graph == NULL){
+  graph* g = read(argv[1]);
+  if(g == NULL){
     printf("Creating a graph failed. Terminating");
     return EXIT_FAILURE;
   }
+  
+  graph_initRandom(g,80,80,1920,1080);
+  /*The graph is now is a legal state. 
+    It is possible to lay it out now
+  */
+  
+  graph_layout(g,1920,1080);
 
-  graph_free(graph);
+  graph_toSVG(g, "test.svg");
+  graph_free(g);
   return EXIT_SUCCESS;
 
   
