c1844878d153b25798024f2008bd2c024902dfc7
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2013-05-06 15:16:00 +1200

    Need to put the parameters somewhere common

---
+++ common.h
@@ -0,0 +1,33 @@
+/*
+ * common.h
+ *
+ *  Created on: May 6, 2013
+ *      Author: roma
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+
+typedef struct {
+  int width;
+  int height;
+  int iterations;
+  int forcemode;
+  float ke;
+  float kh;
+  float kl;
+  float kw;
+  float mass;
+  float time;
+  float coefficientOfRestitution;
+  float mus;
+  float muk;
+  float kg;
+  float wellMass;
+  float edgeCharge;
+} layout_params;
+
+
+
+#endif /* COMMON_H_ */
