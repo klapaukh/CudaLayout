5b39f1f0b8c54ad15b07d6eaff9da4bdfe6c1834
Roma Klapaukh <rklapaukh@hotmail.com>
2013-05-25 11:32:01 +1200

    Added functionality to generate binary trees either by specifying the depth or the number of nodes (to create a balanced complete tree)

--- graphGen.py
+++ graphGen.py
@@ -1,5 +1,13 @@
 from __future__ import print_function
 import sys
+import math
+
+
+def pow(a, b):
+    total = 1
+    for i in range(b):
+        total *= a
+    return total
 
 def circle(nnodes):
     for i in range(nnodes):
@@ -14,17 +22,18 @@ def k(n):
                 i, j))
 def grid(n):
     for i in range(n):
-	for j in range(n):
-  	    print('	<node id="n{0}n{1}" />'.format(i,j))
-#	    if i > 0:
-#		print('	<edge id="e{0}n{1}--{2}n{1}" source="n{0}n{1}" target="n{2}n{1}" />'.format(i,j,i-1))
-	    if i < n-1:
-		print('	<edge id="e{0}n{1}--{2}n{1}" source="n{0}n{1}" target="n{2}n{1}" />'.format(i,j,i+1))
-#	    if j > 0:
-#		print('	<edge id="e{0}n{1}--{0}n{2}" source="n{0}n{1}" target="n{0}n{2}" />'.format(i,j,j-1))
-	    if j < n-1:
-		print('	<edge id="e{0}n{1}--{0}n{2}" source="n{0}n{1}" target="n{0}n{2}" />'.format(i,j,j+1))
-
+        for j in range(n):
+           print('	<node id="n{0}n{1}" />'.format(i,j))
+           if i < n-1:
+               print('	<edge id="e{0}n{1}--{2}n{1}" source="n{0}n{1}" target="n{2}n{1}" />'.format(i,j,i+1))
+           if j < n-1:
+               print('	<edge id="e{0}n{1}--{0}n{2}" source="n{0}n{1}" target="n{0}n{2}" />'.format(i,j,j+1))
+def binaryTree(n):
+    for i in range(n): 
+        print(' <node id="n{0}" />'.format(i))
+        parent = math.floor((i-1)/2)
+        if parent >=0:
+            print(' <edge id="e{0}--{1}" source="n{0}" target="n{1}" />'.format(i,parent))
 
 nnodes = 5
 if len(sys.argv) > 1:
@@ -34,8 +43,12 @@ if len(sys.argv) > 1:
     elif sys.argv[1] == '-k':
         gen = k
     elif sys.argv[1] == '-g':
-	gen = grid
-
+        gen = grid
+    elif sys.argv[1] == '-b':
+        gen = binaryTree
+    elif sys.argv[1] == '-bf':
+        gen = binaryTree
+        nnodes = pow(2,nnodes+1) - 1
 print('<?xml version="1.0"?>')
 print('<graphml>')
 print(' <graph id="g">')
