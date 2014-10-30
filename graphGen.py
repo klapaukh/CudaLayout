e8d486119f9dd642fb2d5bd438b56c6bd92a876e
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2014-10-30 14:39:29 +1300

    Copyright and make circle default

--- graphGen.py
+++ graphGen.py
@@ -1,22 +1,20 @@
-'
- Cuda Graph Layout Tool
+# Cuda Graph Layout Tool
+#
+# Copyright (C) 2013 Roman Klapaukh and Michael Homer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
- Copyright (C) 2013 Roman Klapaukh
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-'
 
 from __future__ import print_function
 import sys
@@ -56,6 +54,7 @@ def binaryTree(n):
             print(' <edge id="e{0}--{1}" source="n{0}" target="n{1}" />'.format(i,parent))
 
 nnodes = 5
+gen = circle
 if len(sys.argv) > 1:
     nnodes = int(sys.argv[2])
     if sys.argv[1] == '-c':
